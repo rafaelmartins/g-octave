f7702480517b06f8962fe98ff93895208c1a91f9
Rafael G. Martins <rafael@rafaelmartins.eng.br>
2009-12-21 00:24:03 -0200

    renamed g-octave/ to g_octave/ #fail :(

---
+++ g_octave/__init__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+__all__ = [
+    'config',
+    'description',
+    'description_tree',
+    'ebuild',
+    'fetch',
+    'overlay'
+]
+
+__author__ = 'Rafael Goncalves Martins'
+__email__ = 'rafael@rafaelmartins.eng.br'
+
+__description__ = 'A tool for Gentoo Linux that generates ebuilds and' + \
+    'installs Octave-Forge packages "on-the-fly"'
+__url__ = 'http://rafaelmartins.eng.br/en-us/projects/g-octave/' # not done yet :(
+__copyright__ = '(c) 2009 %s' % __author__
+__license__ = 'GPLv2'
+
+__version__ = '0.1'
