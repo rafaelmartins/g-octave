c39ff2b6deea8e65e6c5dc59689799881fd4022d
Rafael G. Martins <rafael@rafaelmartins.eng.br>
2009-12-22 02:13:33 -0200

    added setup.py changed etc/g-octave.cfg.devel removed share/info.json

---
+++ setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+import warnings
+warnings.filterwarnings('ignore')
+
+from distutils.core import setup
+import g_octave
+
+setup(
+    name='g-octave',
+    version=g_octave.__version__,
+    license=g_octave.__license__,
+    description=g_octave.__description__,
+    long_description=g_octave.__doc__,
+    author=g_octave.__author__,
+    author_email=g_octave.__email__,
+    url=g_octave.__url__,
+    packages=['g_octave'],
+    scripts=['scripts/g-octave'],
+    data_files=[('/etc', ['etc/g-octave.cfg'])]
+)
