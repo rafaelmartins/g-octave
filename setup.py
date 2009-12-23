59c01e758526e5e6c835142828329c981958ec52
Rafael G. Martins <rafael@rafaelmartins.eng.br>
2009-12-23 17:08:35 -0200

    added LICENSE added MANIFEST.in changed .hgignore changed setup.py

--- setup.py
+++ setup.py
@@ -8,14 +8,15 @@ import g_octave
 
 setup(
     name='g-octave',
-    version=g_octave.__version__,
-    license=g_octave.__license__,
-    description=g_octave.__description__,
-    long_description=g_octave.__doc__,
-    author=g_octave.__author__,
-    author_email=g_octave.__email__,
-    url=g_octave.__url__,
-    packages=['g_octave'],
-    scripts=['scripts/g-octave'],
-    data_files=[('/etc', ['etc/g-octave.cfg'])]
+    version = g_octave.__version__,
+    license = g_octave.__license__,
+    description = g_octave.__description__,
+    long_description = open('README.rst').read(),
+    author = g_octave.__author__,
+    author_email = g_octave.__email__,
+    url = g_octave.__url__,
+    packages = ['g_octave'],
+    scripts = ['scripts/g-octave'],
+    data_files = [('/etc', ['etc/g-octave.cfg'])],
+    requires = ['simplejson', 'portage']
 )
