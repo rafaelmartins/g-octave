1ae9a2e4869afdf2aa10de83f84e811491f1b6f2
Rafael G. Martins <rafael@rafaelmartins.eng.br>
2009-12-23 05:04:04 -0200

    started the README.rst

--- g_octave/__init__.py
+++ g_octave/__init__.py
@@ -14,7 +14,7 @@ __email__ = 'rafael@rafaelmartins.eng.br'
 
 __description__ = """\
 A tool that generates and installs ebuilds for Octave-Forge packages "on-the-fly"
-on Gentoo Linux, using Portage.
+to Gentoo Linux, using Portage.
 """
 
 __url__ = 'http://rafaelmartins.eng.br/en-us/projects/g-octave/' # not done yet :(
