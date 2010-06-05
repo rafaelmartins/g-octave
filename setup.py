66c5d3a2abf6e544e8a8b2590db9b64af0687578
Rafael G. Martins <rafael@rafaelmartins.eng.br>
2010-06-05 02:51:52 -0300

    fixes in the "build system"

--- setup.py
+++ setup.py
@@ -128,7 +128,7 @@ setup(
     author = g_octave.__author__,
     author_email = g_octave.__email__,
     url = g_octave.__url__,
-    packages = ['g_octave', 'g_octave.tinderbox'],
+    packages = ['g_octave'],
     scripts = ['scripts/g-octave'],
     data_files = [('/etc', ['etc/g-octave.cfg'])],
     requires = ['portage'],
