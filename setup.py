70bb0451ee3812289f1ad4f2ebafdb4bb2c60397
Rafael G. Martins <rafael@rafaelmartins.eng.br>
2010-07-01 19:18:39 -0300

    re-added svn stuff to the setup.py script

--- setup.py
+++ setup.py
@@ -128,13 +128,19 @@ setup(
     author = g_octave.__author__,
     author_email = g_octave.__email__,
     url = g_octave.__url__,
-    packages = ['g_octave'],
+    packages = [
+        'g_octave',
+        'g_octave.svn',
+    ],
     scripts = ['scripts/g-octave'],
     data_files = [
         ('/etc', ['etc/g-octave.cfg']),
         ('share/g-octave', ['share/g-octave.eclass']),
     ],
-    requires = ['portage'],
+    requires = [
+        'portage',
+        'pysvn',
+    ],
     cmdclass = {
         'sdist': sdist,
         'build': build,
